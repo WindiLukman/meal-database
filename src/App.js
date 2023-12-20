43a54f88dc77adea95e5da995038d9862096339b
anadidjaja <154343882+anadidjaja@users.noreply.github.com>
2023-12-20 17:22:25 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [searchTerm, setSearchTerm] = useState('');
+    const [recipes, setRecipes] = useState([]);
+
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    useEffect(() => {
+        if (searchTerm !== '') {
+            fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`)
+                .then(response => response.json())
+                .then(data => setRecipes(data.meals));
+        }
+    }, [searchTerm]);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                />
+                {recipes && recipes.map((recipe, index) => (
+                    <div key={index}>
+                        <h2>{recipe.strMeal}</h2>
+                        <img src={recipe.strMealThumb} alt={recipe.strMeal} />
+                    </div>
+                ))}
+            </header>
+        </div>
+    );
 }
 
 export default App;
